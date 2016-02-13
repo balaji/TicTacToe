551c4e1c0b0768aeb738669b6cb58ee21ac3d313
Balaji Damodaran <balaji.damodaran@outlook.com>
2016-02-14 01:42:49 +0530

    high level js project setup done with react. ToDo: build files for both back & frontend

---
+++ public/js/src/app.js
@@ -0,0 +1,9 @@
+// main.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Test to check if all is correct.
+ReactDOM.render(
+    <h1>Tic Tac Toe</h1>,
+    document.getElementById('title')
+);
